cb92a1d4e405aaaaff61a1bee702ef6e0242f972
destroyer3999 <sebnc@outlook.com>
2024-03-15 13:46:51 -0600

    Initial Commit

---
+++ EduQuest/EduQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduQuestApp.swift
+//  EduQuest
+//
+//  Created by Alumno on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct EduQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
